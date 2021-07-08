fca5e4598c631506a3992cc294b73a0fc44c5484
Juan Camilo Valencia Solano <jcvalencias15@gmail.com>
2021-07-08 15:49:09 -0500

    usuarios registrados con exito, pendiente la pagina de login

--- registro/views.py
+++ registro/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import OrderForm, CreateUserForm
+
 
 def registro(request):
 
     return render(request, 'index.html')
-    
+
 
 def register(request):
 
-    return render(request, 'register.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+
+    return render(request, 'register.html', context)
 
 def login(request):
 
-    return render(request, 'login.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+
+    return render(request, 'login.html', context)
